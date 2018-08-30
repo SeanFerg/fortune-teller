345d081035b6a017f5df19cd4b0c88efc0e7c73f
Sean Ferguson <seantoy2010@gmail.com>
2018-08-29 20:50:57 -0400

    final code, but Resource leak: 'input' is never closed

--- src/com/fortuneteller/FortuneTellerApp.java
+++ src/com/fortuneteller/FortuneTellerApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FortuneTellerApp {
 
 	public static void main(String[] args) {
-		// Create scanner to get user input....due 8/30/18 by 9am
+				
 		Scanner input = new Scanner(System.in);
 
 		// Get user first name
@@ -23,101 +23,103 @@ public class FortuneTellerApp {
 		// Get user's birth month (integer)
 		System.out.println("What is your birth month (as a number)?");
 		int birthMonth = input.nextInt();
-		input.nextLine();
-		
+
 		// Get user's favorite color
 		System.out.println("What is your ROYGBIV color or enter help?");
 		String favColor = input.nextLine();
 		if (favColor.equals("help")) {
-		System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet");
-		System.out.println("What is your favorite color?");
-		favColor = input.nextLine();
+			System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet");
+			System.out.println("What is your favorite color?");
+			favColor = input.nextLine();
 		}
 
 		// Get user's number of siblings (integer)
 		System.out.println("How many siblings do you have?");
 		int siblings = input.nextInt();
 
-		//Get user's # of yrs until retirement (based on odd or even age)	
-		if(age % 2 == 0) {
-		System.out.println("You will retire in 18 years");
-		} 
-		else {
-		System.out.println("You will retire in 16 years");
-		}
-	
-	// Vacation Home Location
-	// for (int i =0; i < siblings.length; i++) {
-		if (siblings == 0){
-		System.out.println("a vacation home in Boca Raton, Fl");
-		}
-		if (siblings == 1){
-		System.out.println("a vacation home in Nassau, Bahamas");
-		}
-		if (siblings == 2) {
-		System.out.println("a vacation home in Ponta Negra,Brazil");
-		}
-		if (siblings == 3) {
-		System.out.println("a vacation home in Portland, Oregon");
-		}
-		if (siblings > 3) {
-		System.out.println("a vacation home in Baton Rouge, LA");
-		}
-		if (siblings < 0) {
-		System.out.println("a vacation home in North Korea!!!");
-		}
-	
-		// Mode of transportation
-		if (favColor.equals("red")) {
-		System.out.println("and travel by Maserati");
-		}
-		if (favColor.equals("orange")) 
-		{
-		System.out.println("and travel by stallion");
-		}		
-		if (favColor.equals("yellow"))
-		{
-		System.out.println("and travel by chariot");
-		}
-		if (favColor.equals("green"))
-		{
-		System.out.println("and travel by taxi");
-		}
-		if (favColor.equals("blue"))
-		{
-		System.out.println("and travel by rickshaw");
-		}
-		if (favColor.equals("indigo"))
-		{
-		System.out.println("and travel by motor scooter");
-		}
-		if (favColor.equals("violet"))
-		{
-		System.out.println("and travel by flying saucer");
-		}
+		// Get user's # of yrs until retirement (based on odd or even age)
+		String retire;
 		
-//		Bank Balance
+		if (age % 2 == 0) {
+			retire = "18 years";
+		} else {
+			retire = "16 years";
+		}
+
+		// Vacation Home Location
+		String location = " ";
+
+		if (siblings == 0) {
+			location = "Boca Raton, Fl";
+		}
+
+		if (siblings == 1) {
+			location = "Nassau, Bahamas";
+		}
+
+		if (siblings == 2) {
+			location = "Ponta Negra,Brazil";
+		}
+
+		if (siblings == 3) {
+			location = "Portland, Oregon";
+		}
+
+		if (siblings > 3) {
+			location = "Baton Rouge, LA";
+		}
+
+		if (siblings < 0) {
+			location = "North Korea!!!";
+		}
+
+		// Mode of transportation
+		String transportation = "";
+
+		if (favColor.equals("red")) {
+			transportation = "Maserati";
+		}
+
+		if (favColor.equals("orange")) {
+			transportation = "stallion";
+		}
+
+		if (favColor.equals("yellow")) {
+			transportation = "chariot";
+		}
+
+		if (favColor.equals("green")) {
+			transportation = "taxi";
+		}
+
+		if (favColor.equals("blue")) {
+			transportation = "rickshaw";
+		}
+
+		if (favColor.equals("indigo")) {
+			transportation = "motor scooter";
+		}
+
+		if (favColor.equals("violet")) {
+			transportation = "flying saucer";
+		}
+
+		// Bank Balance
+		double bankBalance = 0.00;
+
 		if (birthMonth < 1 || birthMonth > 12) {
-		System.out.println("balance will be $0.00");
+			bankBalance = (int) 0.00;
+		} else if (birthMonth > 0 && birthMonth < 5) {
+			bankBalance = (int) 250000.00;
+		} else if (birthMonth > 4 && birthMonth < 9) {
+			bankBalance = (int) 3600000.00;
+		} else if (birthMonth > 8 && birthMonth < 13) {
+			bankBalance = (int) 86.00;
 		}
-		else {}
-		if (birthMonth > 0 && birthMonth < 5) {
-		System.out.println("balance will be $250,000.00");
-		}
-		else {}
-		if (birthMonth > 4 && birthMonth < 9) {
-		System.out.println("balance will be $3,600,000.00");
-		}
-		else {}
-		if (birthMonth > 8 && birthMonth < 13) {
-			System.out.println("balance will be $86.00");	
-		}
-		}
+		System.out.println(firstName + " " + lastName + " will retire in " + retire + "" + " with $" + bankBalance
+				+ " in the bank," + " a vacation home in " + location + "" + ", and travel by " + transportation + ".");
+	}
 }
 
-//	for (int i=0; i < siblings; i++) {
-//		total+= siblings[i];
-//	}
-//}
-//System.out.println("The total is " + total);
-//
+//	*[First Name]* *[Last Name]* will retire in *[# of years]* with *[bank balance]* in the bank,
+//	a vacation home in *[location]*, and travel by *[mode of transporation]*.
